9719f505e0ac2d22f9cd9d64f9a433df1d2e1919
Drew, Nicholas <Nicholas.Drew@dbschenker.com>
2018-01-17 16:54:56 +0000

    Tests

---
+++ test/etherWarzRoleManagement.js
@@ -0,0 +1,60 @@
+var SmartDroneCore = artifacts.require("./SmartDroneCore.sol");
+
+contract('SmartDroneCore', function(accounts) {
+
+  it("...should set the EthManager.", function() {
+    return SmartDroneCore.deployed().then(function(instance) {
+      coreInstance = instance;
+      return coreInstance.setEthManager(accounts[1], {from: accounts[0]});
+    }).then(function() {
+      return coreInstance.ethManagerAddress.call();
+    }).then(function(ethAddress) {
+      assert.equal(ethAddress, accounts[1], "The account was not set.");
+    });
+  });
+
+  it("...should set the ConManager.", function() {
+    return SmartDroneCore.deployed().then(function(instance) {
+      coreInstance = instance;
+      return coreInstance.setConManager(accounts[2], {from: accounts[0]});
+    }).then(function() {
+      return coreInstance.conManagerAddress.call();
+    }).then(function(ethAddress) {
+      assert.equal(ethAddress, accounts[2], "The account was not set.");
+    });
+  });
+
+  it("...should set the TokManager.", function() {
+    return SmartDroneCore.deployed().then(function(instance) {
+      coreInstance = instance;
+      return coreInstance.setTokManager(accounts[3], {from: accounts[0]});
+    }).then(function() {
+      return coreInstance.tokManagerAddress.call();
+    }).then(function(ethAddress) {
+      assert.equal(ethAddress, accounts[3], "The account was not set.");
+    });
+  });
+
+  it("...should pause the contract.", function() {
+    return SmartDroneCore.deployed().then(function(instance) {
+      coreInstance = instance;
+      return coreInstance.pause( {from: accounts[0]});
+    }).then(function() {
+      return coreInstance.paused.call();
+    }).then(function(paused) {
+      assert.equal(paused,true, "Contract is stll unpaused");
+    });
+  });
+
+  it("...should unpause the contract.", function() {
+    return SmartDroneCore.deployed().then(function(instance) {
+      coreInstance = instance;
+      return coreInstance.unpause( {from: accounts[0]});
+    }).then(function() {
+      return coreInstance.paused.call();
+    }).then(function(paused) {
+      assert.equal(paused,false, "Contract is stll paused");
+    });
+  });
+
+});
